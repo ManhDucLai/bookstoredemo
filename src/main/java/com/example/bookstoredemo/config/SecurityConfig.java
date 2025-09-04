bb66c42a5734822e05a9e9ee4529054538a55233
laidu <2100829laiducmanh@viu.edu.vn>
2025-09-04 09:05:39 +0700

    Update SecurityConfig.java

--- src/main/java/com/example/bookstoredemo/config/SecurityConfig.java
+++ src/main/java/com/example/bookstoredemo/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // domain FE
+        configuration.setAllowedOrigins(List.of("https://ManhDucLai.github.io/bookstoredemo","http://localhost:5173")); // domain FE
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
